7a3ea4f10ce74fd0d33c4bda902173d7657baf1e
Andrew_Polik <polik.24@mail.ru>
2024-04-13 12:37:23 +0300

    Админка, урлы, и вьюшки для блога

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('pk','title', 'body', 'preview', 'date_is_published','views_count',)
+    search_fields = ('title', 'body',)
+    list_filter = ('date_is_published','views_count',)
